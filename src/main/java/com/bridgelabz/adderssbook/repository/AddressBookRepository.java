751f5e14c0d850730a420f45e5c32f0139167175
unknown <you@example.com>
2022-04-29 21:39:51 +0530

    Added: Updated Table name

--- src/main/java/com/bridgelabz/adderssbook/repository/AddressBookRepository.java
+++ src/main/java/com/bridgelabz/adderssbook/repository/AddressBookRepository.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.adderssbook.repository;
 
-public class AddressBookRepository {
+import com.bridgelabz.addressbook.entity.ContactPerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AddressBookRepository extends JpaRepository<ContactPerson,Long>{
 }
